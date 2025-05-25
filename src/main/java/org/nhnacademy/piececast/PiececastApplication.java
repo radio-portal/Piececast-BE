9ce18c377e70e1fb41e42ff5eecf9283e913a62d
minji <qkralswl6125@naver.com>
2025-05-25 23:39:54 +0900

    Project init

---
+++ src/main/java/org/nhnacademy/piececast/PiececastApplication.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.piececast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiececastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiececastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nhnacademy/piececast/PiececastApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.piececast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiececastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
