bfd7993c1ed45d1807e88d2e73e09ca66f4ebe2d
minji <qkralswl6125@naver.com>
2025-06-09 13:08:48 +0900

    fix: 특정 날짜 조각 리스트 URI 수정

--- src/main/java/org/nhnacademy/piececast/program/controller/ProgramController.java
+++ src/main/java/org/nhnacademy/piececast/program/controller/ProgramController.java
@@ -2,14 +2,12 @@ package org.nhnacademy.piececast.program.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.nhnacademy.piececast.piece.dto.PieceDetailResponse;
 import org.nhnacademy.piececast.program.dto.ProgramCardDto;
 import org.nhnacademy.piececast.program.service.ProgramService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -26,9 +24,9 @@ public class ProgramController {
         return ResponseEntity.ok(programService.getLatestEpisodeCards());
     }
 
-    @GetMapping("/by-date")
+    @GetMapping("/{date}")
     public ResponseEntity<List<ProgramCardDto>> getProgramsByDate(
-            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+            @PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return ResponseEntity.ok(programService.getProgramsByDate(date));
     }
 
