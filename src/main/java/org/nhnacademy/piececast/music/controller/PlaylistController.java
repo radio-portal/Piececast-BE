1f7eb63fd535030b45ca49c9a4359d194febe2c9
minji <qkralswl6125@naver.com>
2025-06-25 04:09:08 +0900

    fix: 선곡표 조회 swagger 설명 추가

--- src/main/java/org/nhnacademy/piececast/music/controller/PlaylistController.java
+++ src/main/java/org/nhnacademy/piececast/music/controller/PlaylistController.java
@@ -1,5 +1,6 @@
 package org.nhnacademy.piececast.music.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.nhnacademy.piececast.music.dto.PlaylistResponse;
 import org.nhnacademy.piececast.music.service.EpisodeMusicService;
@@ -15,6 +16,10 @@ public class PlaylistController {
 
     private final EpisodeMusicService episodeMusicService;
 
+    @Operation(
+            summary = "회차별 음악 재생목록 조회",
+            description = "해당 episodeId에 포함된 음악 목록을 DB에서 꺼내 순서대로 반환합니다."
+    )
     @GetMapping("/{episodeId}/playlist")
     public ResponseEntity<PlaylistResponse> getPlaylist(@PathVariable Long episodeId) {
         List<PlaylistResponse.MusicSimple> playlist = episodeMusicService.getMusicListByEpisodeId(episodeId);
